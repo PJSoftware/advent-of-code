dee9dd3467867a4742b3d2b4fb692cda05a5f6fe
Peter Jones <pjsoftware@petesplace.id.au>
2023-09-01 20:41:18 +1000

    day 3, part 2 -> 2639

--- 2015/03/03.pl
+++ 2015/03/03.pl
@@ -2,55 +2,87 @@
 
 use strict;
 
-
-testSolution('>',2);
-testSolution('^>v<',4);
-testSolution('^v^v^v^v^v',2);
+testSolution('>',2,-1);
+testSolution('^v',-1,3);
+testSolution('^>v<',4,3);
+testSolution('^v^v^v^v^v',2,11);
 print "\n";
 
 my $input = readInput('03-input.txt');
-my $solution = solve($input);
-print "Solution = $solution\n";
+my ($solution1,$solution2) = solve($input);
+print "Solution year 1 = $solution1\n";
+print "Solution year 2 = $solution2\n";
 
 sub testSolution() {
-  my ($input,$exp) = @_;
-  my $got = solve($input);
-  if ($got == $exp) {
-    print "OK '$input' => $got\n";
-  } else {
-    print "** FAIL for '$input':\n";
-    print "  - got $got, exp $exp\n";
-    exit 3;
+  my ($input,$exp1,$exp2) = @_;
+  my ($got1,$got2) = solve($input);
+  if ($exp1 > 0) {
+    if ($got1 == $exp1) {
+      print "OK yr 1 '$input' => $got1\n";
+    } else {
+      print "** FAIL yr 1 for '$input':\n";
+      print "  - got $got1, exp $exp1\n";
+      exit 3;
+    }
+  }
+
+  if ($exp2 > 0) {
+    if ($got2 == $exp2) {
+      print "OK yr 2 '$input' => $got2\n";
+    } else {
+      print "** FAIL yr 2 for '$input':\n";
+      print "  - got $got2, exp $exp2\n";
+      exit 3;
+    }
   }
 }
 
 sub solve {
   my ($input) = @_;
-  my %VISITED = ();
-  my ($x,$y) = (0,0);
+  my %visitedYear1 = ();
+  my %visitedYear2 = ();
 
-  $VISITED{"$x,$y"}++;
+  my %origSanta = ( x => 0, y => 0 );
+  my %santa = ( x => 0, y => 0 );
+  my %robo = ( x => 0, y => 0 );
+
+  my @santas = (\%santa, \%robo);
+  my $active = 0;
+
+  $visitedYear1{"0,0"}++;
+  $visitedYear2{"0,0"}++;
 
   my $ch = '';
   while (length($input) > 0) {
     $ch = substr($input,0,1);
     $input = substr($input,1);
     if ($ch eq '<') {
-      $x++;
+      $origSanta{x}++;
+      $santas[$active]->{x}++;
     } elsif ($ch eq '>') {
-      $x--;
+      $origSanta{x}--;
+      $santas[$active]->{x}--;
     } elsif ($ch eq '^') {
-      $y++;
+      $origSanta{y}++;
+      $santas[$active]->{y}++;
     } elsif ($ch eq 'v') {
-      $y--;
+      $origSanta{y}--;
+      $santas[$active]->{y}--;
     } else {
       print "** Unexpected Character: '$ch'\n";
       exit 4;
     }
-    $VISITED{"$x,$y"}++;
+    $visitedYear1{santaLocation(\%origSanta)}++;
+    $visitedYear2{santaLocation($santas[$active])}++;
+    $active = 1 - $active;
   }
 
-  return scalar(keys %VISITED);
+  return scalar(keys %visitedYear1),scalar(keys %visitedYear2);
+}
+
+sub santaLocation {
+  my $santaPtr = shift;
+  return $santaPtr->{x} . "," . $santaPtr->{y};
 }
 
 sub readInput {
