fc079afce983342a05d6dd5fd2c4438593d945ef
Peter Jones <pjsoftware@petesplace.id.au>
2023-09-01 22:07:21 +1000

    day 4, part 2 -> 1038736

--- 2015/04/04.pl
+++ 2015/04/04.pl
@@ -12,24 +12,25 @@ my %tests = (
 );
 
 foreach my $testInput (sort keys %tests) {
-  Advent::test($testInput, solve($testInput), $tests{$testInput});
+  Advent::test($testInput, solve($testInput, 5), $tests{$testInput});
 }
 print "\n";
 
 my $input = Advent::readBlock('04-input.txt');
-Advent::solution(solve($input));
+Advent::solution(solve($input, 6));
 
 ### solver ###
 
 sub solve {
-  my ($input) = @_;
+  my ($input, $numZeroes) = @_;
 
+  my $prefix = '0' x $numZeroes;
   my $rv = 0;
   my $md5 = '';
   do {
     $rv++;
     $md5 = md5_hex("$input$rv");
-  } until substr($md5,0,5) eq '00000';
+  } until substr($md5,0,$numZeroes) eq $prefix;
 
   return $rv;
 }
