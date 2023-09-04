2409e0c9d5413a1148178a238cd4ab5c54424ba9
Peter Jones <pjsoftware@petesplace.id.au>
2023-09-04 22:53:56 +1000

    day 8, part 2 -> 2074

--- 2015/08/08.pl
+++ 2015/08/08.pl
@@ -12,12 +12,14 @@ my @tests = qw{
   "\x27" 
 };
 
-Advent::test('4 strings', solve(@tests), 12);
+Advent::test('decoder', solveDecode(@tests), 12);
+Advent::test('encoder', solveEncode(@tests), 19);
 
 my @input = Advent::readArray('08-input.txt');
-Advent::solution(solve(@input));
+Advent::solution(solveDecode(@input),'decode');
+Advent::solution(solveEncode(@input),'encode');
 
-sub solve {
+sub solveDecode {
   my @input = @_;
 
   my ($escaped, $actual) = (0,0);
@@ -28,6 +30,22 @@ sub solve {
     $xLine =~ s{"$}{};
     $xLine =~ s{\\\\|\\"|\\x[0-9a-f]{2}}{.}g;
     $actual += length($xLine);
+  }
+
+  return $escaped - $actual;
+}
+
+sub solveEncode {
+  my @input = @_;
+
+  my ($escaped, $actual) = (0,0);
+  foreach my $line (@input) {
+    $actual += length($line);
+    my $xLine = $line;
+    $xLine =~ s{\\}{\\\\}g;
+    $xLine =~ s{"}{\\"}g;
+    $xLine = "\"$xLine\"";
+    $escaped += length($xLine);
   }
 
   return $escaped - $actual;
