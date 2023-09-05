328ddfeaf95867b3abc6b010fe48a2d332217fe4
Peter Jones <pjsoftware@petesplace.id.au>
2023-09-06 00:37:02 +1000

    day 10, part 2 -> 5103798

--- 2015/10/10.pl
+++ 2015/10/10.pl
@@ -14,19 +14,21 @@ my %tests = (
 );
 
 foreach my $testInput (sort keys %tests) {
-  Advent::test($testInput, solve($testInput), $tests{$testInput});
+  Advent::test($testInput, solveLinear($testInput), $tests{$testInput});
 }
 
 my $input = Advent::readBlock('10-input.txt');
 my $answer;
-for my $i (1..40) {
-  $answer = solve($input);
+for my $i (1..50) {
+  $answer = solveLinear($input);
   $input = $answer;
   print "$i -> ".length($answer)."\n";
 }
 Advent::solution(length($answer));
 
-sub solve {
+## solveRecursive() fails with an 'Out of memory!' error while attempting to
+## calculate the 42nd result in the sequence. The length of #41 id 469694.
+sub solveRecursive {
   my ($input) = @_;
 
   if ($input eq '') {
@@ -38,9 +40,27 @@ sub solve {
     my $seq = $2;
     my $rem = $3;
     my $len = length("$ch$seq");
-    return "$len$ch".solve($rem);
+    return "$len$ch".solveRecursive($rem);
   }
 
   print "Could not parse '$input'\n";
   exit 1;
+}
+
+sub solveLinear {
+  my ($input) = @_;
+
+  my $rv = '';
+  my ($ch,$seq,$rem);
+  do {
+    if ($input =~ m{^(\d)(\1*)(.*)}) {
+      $ch = $1;
+      $seq = $2;
+      $rem = $3;
+      my $len = length("$ch$seq");
+      $rv .= "$len$ch";
+      $input = $rem;
+    }
+  } while $rem ne '';
+  return $rv;
 }
