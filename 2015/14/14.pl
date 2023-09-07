92b4a47a68104df2a485f33b4ecf7935ecd9985a
Peter Jones <pjsoftware@petesplace.id.au>
2023-09-07 16:39:06 +1000

    day 14, part 1 -> (2530 is not correct)

--- 2015/14/14.pl
+++ 2015/14/14.pl
@@ -37,9 +37,62 @@ Advent::solution(runRace(\%racers, $raceDuration));
 
 sub registerReindeer {
   my ($dbRef, $stats) = @_;
+
+  if ($stats =~ m{(\S+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds}) {
+    my ($name,$fSpeed,$fDuration,$rDuration) = ($1,$2,$3,$4);
+    $dbRef->{$name}{flightSpeed} = $fSpeed;
+    $dbRef->{$name}{flightDuration} = $fDuration;
+    $dbRef->{$name}{restDuration} = $rDuration;
+    $dbRef->{$name}{state} = 'ready';
+    $dbRef->{$name}{duration} = 0;
+    $dbRef->{$name}{distance} = 0;
+  } else {
+    print "Input not recognised: $stats\n";
+    exit 1;
+  }
 }
 
 sub runRace {
   my ($dbRef, $duration) = @_;
-  return 0;
+
+  foreach my $second (1 .. $duration) {
+    foreach my $racer (sort keys %{$dbRef}) {
+      if ($dbRef->{$racer}{state} eq 'ready') {
+        $dbRef->{$racer}{state} = 'flying';
+        $dbRef->{$racer}{duration} = $dbRef->{$racer}{flightDuration};
+      }
+
+      if ($dbRef->{$racer}{state} eq 'flying') {
+        if ($dbRef->{$racer}{duration} > 0) {
+          $dbRef->{$racer}{duration}--;
+          $dbRef->{$racer}{distance} += $dbRef->{$racer}{flightSpeed};
+        } else {
+          $dbRef->{$racer}{state} = 'resting';
+          $dbRef->{$racer}{duration} = $dbRef->{$racer}{restDuration};
+        }
+      } elsif ($dbRef->{$racer}{state} eq 'resting') {
+        if ($dbRef->{$racer}{duration} > 0) {
+          $dbRef->{$racer}{duration}--;
+        } else {
+          $dbRef->{$racer}{state} = 'flying';
+          $dbRef->{$racer}{duration} = $dbRef->{$racer}{flightDuration};
+        }
+      }
+
+    }
+  }
+  return winner($dbRef);
+}
+
+sub winner {
+  my ($dbRef) = @_;
+  my ($winner,$dist) = ('',0);
+
+  foreach my $racer (sort keys %{$dbRef}) {
+    if ($dbRef->{$racer}{distance} > $dist) {
+      $dist = $dbRef->{$racer}{distance};
+      $winner = $racer;
+    }
+  }
+  return "$winner => $dist";
 }
