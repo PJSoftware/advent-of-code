8828a728da67f857a61dda9c57982aba55a91fed
Peter Jones <pjsoftware@petesplace.id.au>
2023-09-11 11:05:02 +1000

    day 17, part 1 -> 654

--- 2015/17/17.pl
+++ 2015/17/17.pl
@@ -4,13 +4,38 @@ use strict;
 use lib "../lib";
 
 use Advent;
+use List::Util qw{ sum };
 
-my @testSizes = qw{ 20 15 10 5 5};
+my @testSizes = qw{ 20 15 10 5 5 };
 my $testVolume = 25;
 my $testOptions = 4;
 
+
 ##############################################################################
-print "Tests:\n";
+print "Tests: generateCombosWithTarget\n";
+
+my @results = ();
+
+@results = Advent::generateCombinationsWithTarget(10,(10));
+print "Num of results = ".@results."\n";
+foreach my $result (@results) {
+  print " - @{$result}\n";
+}
+
+@results = Advent::generateCombinationsWithTarget(10,(7,3,3));
+print "Num of results = ".@results."\n";
+foreach my $result (@results) {
+  print " - @{$result}\n";
+}
+
+@results = Advent::generateCombinationsWithTarget($testVolume,@testSizes);
+print "Num of results = ".@results."\n";
+foreach my $result (@results) {
+  print " - @{$result}\n";
+}
+
+##############################################################################
+print "Tests: (@testSizes) => $testVolume\n";
 my $DEBUG = 1;
 
 Advent::test("options", solve($testVolume, @testSizes), $testOptions);
@@ -20,11 +45,17 @@ print "\n";
 ##############################################################################
 
 my @containers = Advent::readArray('17-input.txt');
-my $volume = 150;
-Advent::solution(solve($volume, @containers));
+my $targetVolume = 150;
+Advent::solution(solve($targetVolume, @containers));
 
 sub solve {
-  my ($vol, @available) = @_;
+  my ($target, @data) = @_;
+  my @solution = Advent::generateCombinationsWithTarget($target,@data);
+  # print "Num of results = ".@solution."\n";
+  # foreach my $result (@solution) {
+  #   my $sum = sum(@{$result});
+  #   print " - sum(@{$result}) = $sum\n";
+  # }
 
-  return 0;
+  return scalar(@solution);
 }
