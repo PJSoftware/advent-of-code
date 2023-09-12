d10dc1b0d6438a8ef032cd42c271bfc43f3b5fdb
Peter Jones <pjsoftware@petesplace.id.au>
2023-09-12 11:57:47 +1000

    day 19, part 1 -> 518

--- 2015/19/19.pl
+++ 2015/19/19.pl
@@ -30,12 +30,13 @@ Advent::test("calibration HOHOHO", scalar(keys %testOutput), $testDistinctHOHOHO
 
 $DEBUG = 0;
 print "\n";
+
 ##############################################################################
 
 my @input = Advent::readArray('19-input.txt');
 my ($startMolecule, %replacements) = parseInput(@input);
 my %output = calibrate($startMolecule, %replacements);
-Advent::solution(scalar(keys %testOutput),"number of unique molecules");
+Advent::solution(scalar(keys %output),"number of unique molecules");
 
 ##############################################################################
 
@@ -45,6 +46,17 @@ sub parseInput {
   my %replacement = ();
   my $molecule = '';
 
+  foreach my $line (@input) {
+    next unless $line;
+    if ($line =~ /^([A-Z][a-z]?) => ([A-za-z]+)$/) {
+      my ($src,$trg) = ($1,$2);
+      $replacement{$src} = () unless $replacement{$src};
+      push(@{$replacement{$src}},$trg);
+    } else {
+      $molecule = $line; 
+    }
+  }
+
   return($molecule,%replacement);
 }
 
@@ -52,5 +64,39 @@ sub calibrate {
   my ($molecule, %repDB) = @_;
 
   my %output = ();
+  my @molStructure = deconstruct($molecule);
+  my $numAtoms = scalar(@molStructure);
+
+  my $repCount = 0;
+  foreach my $src (sort keys %repDB) {
+    foreach my $trg (sort @{$repDB{$src}}) {
+      foreach my $elIndex (0 .. $numAtoms-1) {
+        if ($molStructure[$elIndex] eq $src) {
+          my @dup = @molStructure;
+          $dup[$elIndex] = $trg;
+          my $newMolecule = construct(@dup);
+          $output{construct(@dup)}++;
+          $repCount++;
+        }
+      }
+    }
+  }
+  print "Total $repCount replacements, ".scalar(keys %output)." distinct molecules\n";
   return %output;
+}
+
+sub deconstruct {
+  my ($molecule) = @_;
+  my @structure = ();
+  while ($molecule =~ /^([A-Z][a-z]?)(.*)/) {
+    my $element = $1;
+    $molecule = $2;
+    push(@structure, $element);
+  }
+  return @structure;
+}
+
+sub construct {
+  my (@structure) = @_;
+  return join('',@structure);
 }
