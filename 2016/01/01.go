7a6f7326df1b48a5ecd9d147d64d97788f9d5d17
Peter Jones <pjsoftware@petesplace.id.au>
2023-10-07 22:24:37 +1000

    day 01, part 2 -> 116

--- 2016/01/01.go
+++ 2016/01/01.go
@@ -11,21 +11,37 @@ import (
 )
 
 var TestsFailed int
+var Visited []string
 
 func main() {
   TestSampleData("sample1","R2, L3",5)
   TestSampleData("sample2","R2, R2, R2",2)
   TestSampleData("sample3","R5, L5, R5, R3",12)
+
+  TestDoubleVisit("doubleVisit","R8, R4, R4, R8",4)
+
   BailOnFail()
 
   input := advent.InputString("01")
-  fmt.Printf("Solution: %d\n",Solve(input))
+  fmt.Printf("Solution 1: %d\n",Solve(input))
+  fmt.Printf("Solution 2: %d\n",FirstRevisitDistance())
 }
 
 // Testing sample data
 
 func TestSampleData(testName string, input string, exp int) {
   got := Solve(input)
+  if got == exp {
+    fmt.Printf("OK -- %s passed\n", testName);
+  } else {
+    fmt.Printf("FAIL -- %s (%s) failed;\n  - exp '%d';\n  - got '%d'\n", testName, input, exp, got)
+    TestsFailed++
+  }
+}
+
+func TestDoubleVisit(testName string, input string, exp int) {
+  _ = Solve(input)
+  got := FirstRevisitDistance()
   if got == exp {
     fmt.Printf("OK -- %s passed\n", testName);
   } else {
@@ -49,6 +65,9 @@ func Solve(input string) int {
   facing := 0
   x := 0
   y := 0
+
+  Visited = nil
+  Visited = append(Visited, fmt.Sprintf("%d,%d",x,y))
 
   for _, instruction := range(instructions) {
     turn := string(instruction[0])
@@ -81,11 +100,34 @@ func Turn(turnDir string, facing int) int {
 }
 
 func Move(x, y int, dir string, dist int) (int,int) {
-  switch dir {
-  case "N": y += dist
-  case "W": x -= dist
-  case "S": y -= dist
-  case "E": x += dist
+  for i := 1; i <= dist; i++ {
+    switch dir {
+    case "N": y++
+    case "W": x--
+    case "S": y--
+    case "E": x++
+    }
+    Visited = append(Visited, fmt.Sprintf("%d,%d",x,y))
   }
   return x, y
+}
+
+func FirstRevisitDistance() int {
+  trackVisit := make(map[string]bool)
+  for _, loc := range(Visited) {
+    if _, ok := trackVisit[loc]; ok {
+      fmt.Printf("Location '%s' has already been visited!\n", loc)
+      return DistTo(loc)
+    }
+    trackVisit[loc] = true
+  }
+
+  return 0
+}
+
+func DistTo(loc string) int {
+  coord := strings.Split(loc,",")
+  x, _ := strconv.Atoi(coord[0])
+  y, _ := strconv.Atoi(coord[1])
+  return int(math.Abs(float64(x)) + math.Abs(float64(y)))
 }
