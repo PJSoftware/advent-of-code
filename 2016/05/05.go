6dd2648e8384837e89abec11553a727d2fe9e424
Peter Jones <pjsoftware@petesplace.id.au>
2023-10-09 16:27:21 +1000

    day 05, part 2 -> 8c35d1ab

--- 2016/05/05.go
+++ 2016/05/05.go
@@ -4,6 +4,8 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/pjsoftware/advent-of-code/2016/lib/advent"
 )
@@ -12,7 +14,9 @@ func main() {
   // Tests
 
   fmt.Print("Starting Tests:\n\n")
-  advent.Test("password generation", "18f47a30", genPassword("abc"))
+  advent.Test("first password generation", "18f47a30", genPassword("abc"))
+  testPwd2 := genSecondPassword("abc")
+  advent.Test("second password generation", "05ace8e3", testPwd2)
   advent.BailOnFail()
   fmt.Print("All tests passed!\n\n");
 
@@ -20,6 +24,8 @@ func main() {
   
   input := advent.InputString("05")
   fmt.Printf("Password for '%s': %s\n",input, genPassword(input))
+
+  genSecondPassword(input)
 }
 
 // Solution code
@@ -39,6 +45,43 @@ func genPassword(seed string) string {
       }
     }
     id++
+  }
+}
+
+func genSecondPassword(seed string) string {
+  id := 0
+
+  fmt.Print("DECRYPTING:\n\n")
+
+  pwdChar := []string{"-","-","-","-","-","-","-","-"}
+  found := 0
+  fmt.Printf("PASSWORD: [ %s ]\r",strings.ToUpper(strings.Join(pwdChar, " ")))
+
+  for {
+    src := fmt.Sprintf("%s%d", seed, id)
+    id++
+    hash := GetMD5Hash(src)
+    if hash[:5] == "00000" {
+      position, err := strconv.Atoi(string(hash[5]))
+      if err != nil {
+        continue
+      }
+      if position > 7 {
+        continue
+      }
+      ch := string(hash[6])
+
+      if pwdChar[position] == "-" {
+        pwdChar[position] = ch
+        found++
+        fmt.Printf("PASSWORD: [ %s ]\r",strings.ToUpper(strings.Join(pwdChar, " ")))
+      }
+    }
+    if found == 8 {
+      pwd := strings.Join(pwdChar,"")
+      fmt.Printf("\n\nPASSWORD FOUND: %s\n",pwd)
+      return pwd
+    }
   }
 }
 
