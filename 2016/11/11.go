9c3941ec7cefe0a391ace8a1086d3bbdc787fdb7
Peter Jones <pjsoftware@petesplace.id.au>
2023-12-07 13:18:49 +1000

    this solution passes the test phase

--- 2016/11/11.go
+++ 2016/11/11.go
@@ -22,13 +22,6 @@ const (
   Down Direction = -1
 )
 
-type Safety int
-const (
-  AlwaysSafe Safety = iota
-  Protected
-  Unprotected
-)
-
 func main() {
   // Tests
 
@@ -45,7 +38,7 @@ func main() {
 
   fmt.Print("All tests passed!\n\n");
   os.Exit(1)
-  
+
   // Solution
 
   layout := NewLayout()
@@ -70,8 +63,13 @@ type Layout struct {
   MinFloor Floor
   MaxFloor Floor
   ElevatorOnFloor Floor
-  ElevatorMove Direction
   ElevatorMoves int
+}
+
+type Move struct {
+  Dir Direction
+  Equip1 *Equip
+  Equip2 *Equip
 }
 
 func NewLayout() *Layout {
@@ -79,9 +77,37 @@ func NewLayout() *Layout {
   l.MinFloor = Floor1
   l.MaxFloor = Floor4
   l.ElevatorOnFloor = Floor1
-  l.ElevatorMove = Parked
   l.ElevatorMoves = 0
   return l
+}
+
+func NewMove(dir Direction, eq1, eq2 *Equip) *Move {
+  m := &Move{}
+  m.Dir = dir
+  m.Equip1 = eq1
+  m.Equip2 = eq2
+  return m
+}
+
+func (m *Move) Describe() string {
+  dir := ""
+  switch m.Dir {
+  case Up: dir = "up"
+  case Down: dir = "down"
+  }
+  if m.Equip2 == nil {
+    return fmt.Sprintf("Move %s %s", m.Equip1.Name(), dir)
+  }
+
+  return fmt.Sprintf("Move %s and %s %s", m.Equip1.Name(), m.Equip2.Name(), dir)
+}
+
+func (l *Layout) Describe() string {
+  desc := fmt.Sprintf("LAYOUT:%d",l.ElevatorOnFloor)
+  for _, eq := range(l.Equipment) {
+    desc += fmt.Sprintf(":%s(%d)", eq.Name(), eq.OnFloor)
+  }
+  return desc
 }
 
 func (l *Layout) AddToFloor(floor Floor, eq *Equip) {
@@ -90,6 +116,147 @@ func (l *Layout) AddToFloor(floor Floor, eq *Equip) {
 }
 
 func (l *Layout) MovesRequired() int {
-  return 1
+  layoutSeen := make(map[string]bool)
+  allPossibleLayouts := []*Layout{}
+  allPossibleLayouts = append(allPossibleLayouts, l)
+  moves := 0
+
+  for {
+    nextLayouts := []*Layout{}
+
+    if len(allPossibleLayouts) == 0 {
+      return -1
+    }
+
+    for _, layout := range(allPossibleLayouts) {
+      if layout.IsFinal() {
+        return layout.ElevatorMoves
+      }
+    }
+
+    for _, layout := range(allPossibleLayouts) {
+      if !layoutSeen[layout.Describe()] && layout.IsValid() {
+        layoutSeen[layout.Describe()] = true
+        moves := layout.PossibleMoves()
+        for _, move := range(moves) {
+          nextLayouts = append(nextLayouts, layout.Apply(move))
+        }
+      }
+    }
+
+    allPossibleLayouts = nil
+    allPossibleLayouts = append(allPossibleLayouts, nextLayouts...)
+    moves++
+  }
+
 }
 
+func (l *Layout) IsFinal() bool {
+  for _, eq := range(l.Equipment) {
+    if eq.OnFloor != Floor4 {
+      return false
+    }
+  }
+
+  return true
+}
+
+func (l *Layout) IsValid() bool {
+  chipFloor := make(map[Element]Floor)
+  genFloor := make(map[Element]Floor)
+
+  for _, eq := range(l.Equipment) {
+    if eq.Type == Microchip {
+      chipFloor[eq.Element] = eq.OnFloor
+    } else {
+      genFloor[eq.Element] = eq.OnFloor
+    }
+  }
+
+  for cEl := range(chipFloor) {
+    if genFloor[cEl] != chipFloor[cEl] { // Chip is not protected
+      for gEl := range(chipFloor) {
+        if gEl != cEl {
+          if genFloor[gEl] == chipFloor[cEl] { // Unprotected chip on same floor as another generator
+            return false
+          }
+        }
+      }
+    }
+  }
+
+  return true
+}
+
+// l.Apply() generates a new layout by applying the specified move to the
+// current layout
+func (l *Layout) Apply(move *Move) *Layout {
+  nl := &Layout{}
+  nl.MinFloor = Floor1
+  nl.MaxFloor = Floor4
+  nl.ElevatorOnFloor = l.ElevatorOnFloor + Floor(move.Dir)
+  nl.ElevatorMoves = l.ElevatorMoves + 1
+
+  for _, eq := range(l.Equipment) {
+    floor := eq.OnFloor
+    if move.Equip1.Element == eq.Element && move.Equip1.Type == eq.Type {
+      floor += Floor(move.Dir)
+    } else if move.Equip2 != nil && move.Equip2.Element == eq.Element && move.Equip2.Type == eq.Type {
+      floor += Floor(move.Dir)
+    }
+    nl.AddToFloor(floor, NewEquip(eq.Element,eq.Type))
+  }
+
+  return nl
+}
+
+func (l *Layout) PossibleMoves() []*Move {
+  canMove := l.ObjectsWithElevator()
+
+  moves := []*Move{}
+  if len(canMove) == 0 {
+    return moves
+  }
+
+  if len(canMove) == 1 {
+    if l.ElevatorOnFloor < Floor4 {
+      moves = append(moves, NewMove(Up, canMove[0], nil))
+    }
+    if l.ElevatorOnFloor > Floor1 {
+      moves = append(moves, NewMove(Down, canMove[0], nil))
+    }
+    return moves
+  }
+  
+  for _, eq := range(canMove) {
+    if l.ElevatorOnFloor < Floor4 {
+      moves = append(moves, NewMove(Up, eq, nil))
+    }
+    if l.ElevatorOnFloor > Floor1 {
+      moves = append(moves, NewMove(Down, eq, nil))
+    }
+  }
+  
+  for i := 0; i < len(canMove)-1; i++ {
+    for j := i+1; j < len(canMove); j++ {
+      if l.ElevatorOnFloor < Floor4 {
+        moves = append(moves, NewMove(Up, canMove[i], canMove[j]))
+      }
+      if l.ElevatorOnFloor > Floor1 {
+        moves = append(moves, NewMove(Down, canMove[i], canMove[j]))
+      }
+    }
+  }
+
+  return moves
+}
+
+func (l *Layout) ObjectsWithElevator() []*Equip {
+  list := []*Equip{}
+  for _, eq := range(l.Equipment) {
+    if eq.OnFloor == l.ElevatorOnFloor {
+      list = append(list, eq)
+    }
+  }
+  return list
+}
--- 2016/11/equip.go
+++ 2016/11/equip.go
@@ -3,8 +3,7 @@ package main
 type Element int
 
 const (
-	NoElement Element = iota
-	Hydrogen
+	Hydrogen Element = iota
 	Lithium
 	Strontium
 	Plutonium
@@ -23,7 +22,6 @@ const (
 type Equip struct {
 	Type    EquipType
 	Element Element
-	Safety  Safety
 	OnFloor Floor
 }
 
@@ -31,10 +29,35 @@ func NewEquip(el Element, et EquipType) *Equip {
 	eq := &Equip{}
 	eq.Element = el
 	eq.Type = et
-	if et == Generator {
-		eq.Safety = AlwaysSafe
-	} else {
-		eq.Safety = Unprotected
-	}
 	return eq
+}
+
+func (eq *Equip) Name() string {
+	name := ""
+
+	switch eq.Element {
+	case Hydrogen:
+		name = "hydrogen"
+	case Lithium:
+		name = "lithium"
+	case Strontium:
+		name = "strontium"
+	case Plutonium:
+		name = "plutonium"
+	case Thulium:
+		name = "thulium"
+	case Ruthenium:
+		name = "ruthenium"
+	case Curium:
+		name = "curium"
+	}
+
+	switch eq.Type {
+	case Generator:
+		name += " generator"
+	case Microchip:
+		name += "-compatible microchip"
+	}
+
+	return name
 }
