be4aab5e93c5f713f1109328fc97386c2afa59d0
Peter Jones <pjsoftware@petesplace.id.au>
2023-12-10 18:57:13 +1000

    first pass returned a wrong answer; ranges overlap

--- 2016/20/20.go
+++ 2016/20/20.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strconv"
+	"strings"
 
 	"github.com/pjsoftware/advent-of-code/2016/lib/advent"
 )
@@ -29,5 +32,29 @@ func main() {
 // Solution code
 
 func LowestAllowableIP(input []string) int {
-	return 0
+	blackList := make(map[int]int)
+	for _, row := range input {
+		low, high := parse(row)
+		blackList[low] = high
+	}
+
+	test := 0
+	for {
+		if next, ok := blackList[test]; ok {
+			test = next + 1
+		} else {
+			return test
+		}
+	}
+}
+
+func parse(row string) (int, int) {
+	val := strings.Split(row, "-")
+	if len(val) != 2 {
+		log.Fatalf("Row '%s' does not mach expected nnn-nnn pattern", row)
+	}
+
+	n1, _ := strconv.ParseInt(val[0], 10, 64)
+	n2, _ := strconv.ParseInt(val[1], 10, 64)
+	return int(n1), int(n2)
 }
