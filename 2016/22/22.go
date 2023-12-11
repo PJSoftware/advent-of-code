4953311385157cccdc494f6ca57ba55048250eca
Peter Jones <pjsoftware@petesplace.id.au>
2023-12-11 18:12:19 +1000

    part 2, first pass -- I thought 177 but that's wrong
    When doing my 'visual inspection' of the data, I was transposing x & y
    so potentially there is actually a blockage in the top few rows that we
    need to work around, that I was not aware of. The next step will be to
    display that data so I know what I'm looking at...

--- 2016/22/22.go
+++ 2016/22/22.go
@@ -11,6 +11,7 @@ import (
 
 type Grid struct {
   Nodes map[string]*Node
+  MaxX, MaxY int
 }
 
 func NewGrid() *Grid {
@@ -21,6 +22,8 @@ func NewGrid() *Grid {
 
 func (g *Grid) AddNode(n *Node) {
   g.Nodes[n.Name()] = n
+  if n.X > g.MaxX { g.MaxX = n.X }
+  if n.Y > g.MaxY { g.MaxY = n.Y }
 }
 
 type Node struct {
@@ -50,7 +53,11 @@ func NewNode(input string) *Node {
 }
 
 func (n *Node) Name() string {
-  return fmt.Sprintf("node-x%d-y%d", n.X, n.Y)
+  return nodeName(n.X, n.Y)
+}
+
+func nodeName(x, y int) string {
+  return fmt.Sprintf("node-x%d-y%d", x, y)
 }
 
 func (n *Node) Avail() int {
@@ -62,8 +69,34 @@ func (n *Node) UsePC() int {
 }
 
 func main() {
-  // No sample data provided; pre-testing not possible
-  
+  // Tests
+
+  testData := []string{
+    "Filesystem            Size  Used  Avail  Use%",
+    "/dev/grid/node-x0-y0   10T    8T     2T   80%",
+    "/dev/grid/node-x0-y1   11T    6T     5T   54%",
+    "/dev/grid/node-x0-y2   32T   28T     4T   87%",
+    "/dev/grid/node-x1-y0    9T    7T     2T   77%",
+    "/dev/grid/node-x1-y1    8T    0T     8T    0%",
+    "/dev/grid/node-x1-y2   11T    7T     4T   63%",
+    "/dev/grid/node-x2-y0   10T    6T     4T   60%",
+    "/dev/grid/node-x2-y1    9T    8T     1T   88%",
+    "/dev/grid/node-x2-y2    9T    6T     3T   66%",
+  }
+
+  testGrid := NewGrid()
+  for _, data := range testData {
+    n := NewNode(data)
+    if n != nil {
+      testGrid.AddNode(n)
+    }
+  }
+  advent.Test("nodes loaded correctly", 9, len(testGrid.Nodes))
+  advent.Test("viable pairs", 7, testGrid.CountViablePairs()) // 7, apparently!
+  advent.Test("steps to retrieve", 7, testGrid.RetrievePayload())
+  advent.BailOnFail()
+  fmt.Print("All tests passed!\n\n");
+
   input := advent.InputStrings("22")
 
   g := NewGrid()
@@ -75,22 +108,70 @@ func main() {
   }
   fmt.Printf("%d nodes imported into grid\n", len(g.Nodes))
 
-  fmt.Printf("Valid Pairs: %d\n",g.CountValidPairs())
+  fmt.Printf("Viable Pairs: %d\n",g.CountViablePairs())
+  fmt.Printf("Steps to retrieve: %d\n",g.RetrievePayload())
 }
 
 // Solution code
 
-func (g *Grid) CountValidPairs() int {
-  valid := 0
+func (g *Grid) CountViablePairs() int {
+  viable := 0
   for nameA, nodeA := range g.Nodes {
     for nameB, nodeB := range g.Nodes {
       if nameA == nameB { continue }
       if nodeA.Used > 0 && nodeB.Avail() >= nodeA.Used {
-        valid++
+        viable++
       }
     }
   }
-  return valid
+  return viable
+}
+
+// RetrievePayload() approach: A quick visual inspection of the data shows that
+// it should be possible to "move" the empty node (from position 26,22) just to
+// the left of the payload position (to 30, 0), then "walk" the payload along
+// the top row to the target position.
+//
+// Estimated number of moves required: 4 + 22 + 30*(1+4) + 1 = 177
+//
+// Actual formula:
+//   (payload.X-1)-empty.X 
+//   + empty.Y-payload.Y
+//   + (payload.x-1)*5 + 1
+func (g *Grid) RetrievePayload() int {
+  payload := g.RetrieveNodeByCoordinates(g.MaxX, 0)
+  empty := g.RetrieveEmptyNode()
+
+  fmt.Printf("Target data %dT @ (%d,%d)\n", payload.Used, payload.X, payload.Y)
+  fmt.Printf("Empty node %dT @ (%d,%d)\n", empty.Used, empty.X, empty.Y)
+
+  // Rather than actually moving the data (just yet) let's just calculate the
+  // formula
+  //
+  // This is possible because a simple glance at the data shows no obvious
+  // roadblocks to actually performing the operation (although I'm sure Step 2
+  // will change that! (Oh wait; this is step 2!)) Since all we want is the
+  // number of steps, we should be able to calculate that directly.
+  stepsToMoveEmpty := empty.Y-payload.Y + payload.X-empty.X-1
+  stepsToWalkPayload := (payload.X-1)*5 + 1
+  return stepsToMoveEmpty + stepsToWalkPayload
+}
+
+func (g *Grid) RetrieveNodeByCoordinates(x, y int) *Node {
+  name := nodeName(x, y)
+  if node, ok := g.Nodes[name]; ok {
+    return node
+  }
+  return nil
+}
+
+func (g *Grid) RetrieveEmptyNode() *Node {
+  for _, node := range g.Nodes {
+    if node.Used == 0 {
+      return node
+    }
+  }
+  return nil
 }
 
 func convertToInt(s string) int {
