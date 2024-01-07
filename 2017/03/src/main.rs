f0a65d5656bd6dd367da780df9ef39f1cac05f95
Peter Jones <pjsoftware@petesplace.id.au>
2024-01-07 23:46:48 +1000

    day 03, part 2 -> 295229

--- 2017/03/src/main.rs
+++ 2017/03/src/main.rs
@@ -14,13 +14,28 @@ fn main() {
     ("23", 2),
     ("1024", 31),
   ]);
+  let test_data2 = HashMap::from([
+    ("1", 2),
+    ("4", 5),
+    ("9", 10),
+    ("16", 23),
+    ("25", 26),
+    ("12", 23),
+    ("23", 25),
+  ]);
+
   let mut test = advent::Test::new();
 
   println!("Starting Tests:\n");
   for (key, val) in test_data.iter() {
-    let label = format!("sample: {}", key);
+    let label = format!("#1 sample: {}", key);
     let test_int = key.parse::<i32>().unwrap();
     test.test_i32(&label, *val, manhattan_distance(test_int));
+  }
+  for (key, val) in test_data2.iter() {
+    let label = format!("#2 sample: {}", key);
+    let test_int = key.parse::<i32>().unwrap();
+    test.test_i32(&label, *val, stress_test(test_int));
   }
   test.bail_on_fail();
   println!("All tests passed!\n");
@@ -29,7 +44,9 @@ fn main() {
 
   let input = advent::read_i32("03-input.txt");
   let solution = manhattan_distance(input);
+  let solution2 = stress_test(input);
   println!("Manhattan Distance: {}", solution);
+  println!("Stress Test: {}", solution2);
 }
 
 fn manhattan_distance(mut data: i32) -> i32 {
@@ -89,4 +106,87 @@ fn manhattan_distance(mut data: i32) -> i32 {
 
   }
   return x.abs() + y.abs();
+}
+
+fn stress_test(data: i32) -> i32 {
+  let mut memory = HashMap::from([
+    (memory_key(0,0), 1),
+  ]);
+
+  let mut d1 = 1;
+  let mut d2 = 1;
+
+  let mut x = 0;
+  let mut y = 0;
+
+  let mut value;
+
+  loop {
+
+    // Right
+    for _n in 1..d1+1 {
+      x += 1;
+      value = determine_value(x, y, &memory);
+      memory.insert(memory_key(x,y), value);
+      if value > data {
+        return value;
+      }      
+    }
+    d1 += 1;
+
+    // Up
+    for _n in 1..d2+1 {
+      y += 1;
+      value = determine_value(x, y, &memory);
+      memory.insert(memory_key(x,y), value);
+      if value > data {
+        return value;
+      }      
+    }
+    d2 += 1;
+
+    // Left
+    for _n in 1..d1+1 {
+      x -= 1;
+      value = determine_value(x, y, &memory);
+      memory.insert(memory_key(x,y), value);
+      if value > data {
+        return value;
+      }      
+    }
+    d1 += 1;
+
+    // Down
+    for _n in 1..d2+1 {
+      y -= 1;
+      value = determine_value(x, y, &memory);
+      memory.insert(memory_key(x,y), value);
+      if value > data {
+        return value;
+      }      
+    }
+    d2 += 1;
+
+  }
+}
+
+fn memory_key(x: i32, y:i32) -> String {
+  let key = format!("({}:{})", x, y);
+  key
+}
+
+fn determine_value(x: i32, y:i32, memory: &HashMap<String, i32>) -> i32 {
+  let mut total: i32;
+  total = 0;
+
+  for ix in -1..1+1 { // for ix in -1..1 only loops through -1, 0 !!!!!
+    for iy in -1..1+1 {
+      let key = memory_key(x+ix, y+iy);
+      if memory.contains_key(&key) {
+        let val = memory.get(&key).unwrap();
+        total += val;
+      }
+    }
+  }
+  total
 }
