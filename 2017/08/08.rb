ed03d549314436b3a564385513e684f385a08adf
Peter Jones <pjsoftware@petesplace.id.au>
2024-01-16 15:43:56 +1000

    day 08, part 2 -> 5199

--- 2017/08/08.rb
+++ 2017/08/08.rb
@@ -2,9 +2,11 @@ require_relative "../lib/advent/read"
 require_relative "../lib/advent/test"
 
 class Program
+  attr_reader :high_value
 
   def initialize
     @register = Hash.new(0)
+    @high_value = 0
   end
 
   def max_value
@@ -58,25 +60,33 @@ class Program
       puts "Modification method '#{mod}' not recognised!"
       exit(2)
     end
+
+    if @register[reg] > @high_value
+      @high_value = @register[reg]
+    end
   end
 
 end
 
-def solve(input)
+def run_program(input)
   prog = Program.new
   input.each do |line|
     prog.run(line)
   end
-  return prog.max_value
+  return prog
 end
 
 puts "Starting Tests:"
 tests = Test.new()
 test_input = Read::strings("input_test.txt")
 test_answer = 1
-tests.test("test data solution", test_answer, solve(test_input))
+test_prog = run_program(test_input)
+tests.test("test data solution", test_answer, test_prog.max_value)
+tests.test("highest test value", 10, test_prog.high_value)
 tests.bail_on_fail()
 puts "All tests passed!\n---"
 
 input = Read::strings("input_data.txt")
-puts "Solution: #{solve(input)}"
+prog = run_program(input)
+puts "Solution: #{prog.max_value}"
+puts "High Value: #{prog.high_value}"
