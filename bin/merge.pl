afde6db4349026077ef038c8af15e1e47b8e37a0
Peter Jones <pjsoftware@petesplace.id.au>
2024-01-16 17:04:59 +1000

    delete old branch on merge

--- bin/merge.pl
+++ bin/merge.pl
@@ -5,8 +5,14 @@ use Git;
 
 my $YEAR = $ARGV[0];
 if (!$YEAR) {
+  print "Year not specified\n";
   exit(1);
 }
 
 my $cb = Git::branch_current();
-system("git checkout $YEAR && git merge $cb --no-ff && git push")
+if ($cb !~ /^day\d\d/) {
+  print "Branch $cb is not a dayNN branch!\n";
+  exit(2);
+}
+
+system("git checkout $YEAR && git merge $cb --no-ff && git push && git branch -d $cb");
