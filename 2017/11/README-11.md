69062579ecf96407eac3b22fbaf465c32071a2a9
Peter Jones <pjsoftware@petesplace.id.au>
2024-01-17 12:34:32 +1000

    day 11, part 1 -> 818

--- 2017/11/README-11.md
+++ 2017/11/README-11.md
@@ -28,3 +28,9 @@ ne,ne,ne is 3 steps away.
 ne,ne,sw,sw is 0 steps away (back where you started).
 ne,ne,s,s is 2 steps away (se,se).
 se,sw,se,sw,sw is 3 steps away (s,s,sw).
+
+### Hexagonal Grids
+
+**Red Blob Games** has an awesome resource on [Hexagonal Grids](https://www.redblobgames.com/grids/hexagons/) which is well worth reading if you have any need to implement one.
+
+What I discovered while reading through it, wondering which was the best way to model my grid, was that my determining distance was slightly easier than I thought -- if my model was correct! 😀
--- 2017/11/hexgrid.rb
+++ 2017/11/hexgrid.rb
@@ -1,33 +1,39 @@
 class HexGrid
 
-  # To model this grid we shall use three axes: x, y, z
-  # x is N/S, with N positive (N is "0 deg" up the page)
-  # y is NE/SW, with NE positive (NE is "60 deg" to the right of N)
-  # z is SE/NW, with SE positive (SE is "120 deg", 60 deg to the right of NE)
+  # To model this grid we shall use cube coordinates per https://www.redblobgames.com/grids/hexagons/
+  # q is N/S, with N positive (N is "0 deg" up the page)
+  # r is SE/NW, with SE positive (SE is "120 deg" to the right of N)
+  # s is SW/NE, with SW positive (SW is "240 deg", 120 deg to the right of SE)
   # This makes our positive direction clockwise.
 
   # The default starting position is (0,0,0)
   def initialize
-    @x = 0
-    @y = 0
-    @z = 0
+    @q = 0
+    @r = 0
+    @s = 0
   end
 
   def follow_path(path)
     path.split(",").each do |move|
       case move
-      when "n"
-        @x += 1
-      when "s"
-        @x -= 1
-      when "ne"
-        @y += 1
-      when "sw"
-        @y -= 1
-      when "se"
-        @z += 1
-      when "nw"
-        @z -= 1
+      when "n" # positive q
+        @r -= 1
+        @s += 1
+      when "s" # negative q
+        @r += 1
+        @s -= 1
+      when "se" # positive r 
+        @q += 1
+        @s -= 1
+      when "nw" # negative r
+        @q -= 1
+        @s += 1
+      when "sw" # positive s
+        @q -= 1
+        @r += 1
+      when "ne" # negative s
+        @q += 1
+        @r -= 1
       else
         puts "WTF is '#{path}'?"
         exit 1
@@ -37,8 +43,8 @@ class HexGrid
 
   # I'm fairly sure this is not correct
   def steps_from_origin
-    puts "Coordinates: (#{@x},#{@y},#{@z})"
-    return @x.abs + @y.abs + @z.abs
+    puts "Coordinates: (#{@q},#{@r},#{@s})"
+    return [@q.abs,@r.abs,@s.abs].max
   end
 
 end
