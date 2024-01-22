f532314e38186327fd3b6ba7e5d43517725c45c8
Peter Jones <pjsoftware@petesplace.id.au>
2024-01-22 10:11:36 +1000

    day 12, part 1 -> 306

--- 2017/12/12.rb
+++ 2017/12/12.rb
@@ -1,8 +1,75 @@
 require_relative "../lib/advent/read"
 require_relative "../lib/advent/test"
 
-def solve(input)
-  return input.length
+class Group
+  def initialize
+    @nodes = Array.new
+  end
+
+  def add_node(data)
+    node = Node.new(data)
+    @nodes.push(node)
+  end
+
+  def set_connected(id)
+    @nodes.each do |node|
+      if node.id == id
+        node.connected = true
+        return
+      end
+    end
+  end
+
+  def count_connected(source)
+    rv = 0
+    loop do
+      mod = 0
+      @nodes.each do |node|
+        if !node.counted
+          if node.id == source || node.connected
+            rv += 1
+            mod += 1
+            node.counted = true
+            node.neighbours.each do |node_id|
+              set_connected(node_id)
+            end
+          end
+        end
+      end
+      if mod == 0
+        return rv
+      end
+    end
+  end
+end
+
+class Node
+  attr_reader :id, :neighbours
+  attr_accessor :counted, :connected
+
+  def initialize(data)
+    comms = data.match(/^(?<id>\d+) \<-\> (?<nn>[\d, ]+)$/)
+    if comms
+      @id = comms[:id].to_i
+      @counted = false
+      @connected = false
+      @neighbours = Array.new
+      comms[:nn].split(", ").each do |neighbour|
+        @neighbours.push(neighbour.to_i)
+      end
+    else
+      fput "Unrecognised comms data: '#{data}"
+    end
+  end
+end
+
+def analyse_data(input)
+  group = Group.new
+  input.each do |data|
+    group.add_node(data)
+  end
+
+  return group.count_connected(0)
 end
 
 puts "Starting Tests:"
@@ -10,10 +77,10 @@ tests = Test.new
 
 test_input = Read::strings("input_test.txt")
 test_answer = 6
-tests.test("test data solution", test_answer, solve(test_input))
+tests.test("test data solution", test_answer, analyse_data(test_input))
 
 tests.bail_on_fail
 puts "All tests passed!\n---"
 
 input = Read::strings("input_data.txt")
-puts "Solution: #{solve(input)}"
+puts "Solution: #{analyse_data(input)}"
