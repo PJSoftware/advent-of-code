0ed95de2d00e088d769a0ee2dceea6a92cee90ab
Peter Jones <pjsoftware@petesplace.id.au>
2024-01-28 22:06:28 +1000

    expected 1242, got 1229 - ???

--- 2017/14/14.rb
+++ 2017/14/14.rb
@@ -4,11 +4,13 @@ require_relative "../lib/advent/test"
 require_relative "../lib/knot_hash"
 
 class Disk
+  attr_reader :grid
 
   def initialize(key)
     @grid = Array.new
     for i in 0..127 do
-      knot = KnotHash.new(key + "-" + i.to_s)
+      k = key + "-" + i.to_s
+      knot = KnotHash.new(k)
       knot.tie
       @grid.push(knot.checksum)
     end
@@ -28,6 +30,52 @@ class Disk
       rv += c.to_i
     end
     rv
+  end
+
+end
+
+class Grid
+  attr_reader :regions
+
+  def initialize(disk)
+    @grid = Array.new
+    @regions = 0
+
+    disk.grid.each do |row|
+      @grid.push(hex_to_bin(row).chars)
+    end
+  end
+  
+  def flood_fill
+    used = "1"
+    empty = "0"
+    for i in 0..127 do
+      for j in 0..127 do
+        if @grid[i][j] == used
+          @regions += 1
+          fill(i,j,used,"#")
+        elsif @grid[i][j] == empty
+          @grid[i][j] = " "
+        end
+      end
+      # puts @grid[i].join()
+    end
+  end
+
+  def fill(i,j,old_ch,new_ch)
+    if i < 0 || i > 127
+      return
+    end
+    if i < 0 || i > 127
+      return
+    end
+    if @grid[i][j] == old_ch
+      @grid[i][j] = new_ch
+      fill(i-1,j,old_ch,"@")
+      fill(i+1,j,old_ch,"@")
+      fill(i,j-1,old_ch,"@")
+      fill(i,j+1,old_ch,"@")
+    end
   end
 
 end
@@ -81,6 +129,10 @@ test_disk = Disk.new(test_input)
 tests.test("hex2bin: A0", "10100000", hex_to_bin("A0"))
 tests.test("hex2bin: A0", "0000111111110001", hex_to_bin("0FF1"))
 tests.test("test num squares", 8108, test_disk.used)
+test_grid = Grid.new(test_disk)
+test_grid.flood_fill
+tests.test("test num regions", 1242, test_grid.regions) ## <- answer given
+# tests.test("test num regions", 1229, test_grid.regions) ## <- actual answer?
 tests.bail_on_fail
 
 puts "All tests passed!\n---"
@@ -88,3 +140,7 @@ puts "All tests passed!\n---"
 input = Read::string("input_data.txt")
 disk = Disk.new(input)
 puts "Squares used: #{disk.used}"
+
+grid = Grid.new(disk)
+grid.flood_fill
+puts "Regions used: #{grid.regions}"
